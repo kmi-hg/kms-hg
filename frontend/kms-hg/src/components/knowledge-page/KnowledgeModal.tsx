71b49c14723d5f5240e507b5ae1a3266d28533ef
KMI-HG <knowledgemanagement.innovation@gmail.com>
2025-05-02 05:32:38 +0700

    fix build error

--- frontend/kms-hg/src/components/knowledge-page/KnowledgeModal.tsx
+++ frontend/kms-hg/src/components/knowledge-page/KnowledgeModal.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import SuccessModal from "./SuccessModal"; // Import SuccessModal
 
 const Modal = ({
